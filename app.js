f7c57ed4f346fc555e238939fa3c4f8a44143314
LeedWover <aresone.gaben@gmail.com>
2019-04-13 17:09:11 +0200

    api endpoint

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+    type RootMutation {
+      createEvent(name: String): String
+    }
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['Romantic cooking', 'Sailing', 'All night coding']
+    },
+    createEvent: args => {
+      const eventName = args.name;
+      return eventName;
+    }
+  },
+  graphiql: true
+}));
 
 app.listen(5000, () => console.log('Server started!'));
